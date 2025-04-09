93b6a9d669e1e3abbf61f5b7f107a1c8a7238b45
poloite <yomn5385@gmail.com>
2025-04-09 13:53:29 +0900

    자료구조6주차

--- js/search.js
+++ js/search.js
@@ -11,4 +11,5 @@ function googleSearch() {
     // 새 창에서 구글 검색을 수행
     window.open(googleSearchUrl, "_blank"); // 새로운 창에서 열기.
     return false;
-    }
+}
+//문자 길이가 0이거나 null인지 확인하는 함수 사용, 배열로 욕설 입력하고 일치하는 함수가 있으면 함수 중단.
