d8236dea2a4420269359fa985a0489ae2945bbb1
poloite <yomn5385@gmail.com>
2025-04-16 13:55:18 +0900

    자바웹 7주차차

--- js/pop_up.js
+++ js/pop_up.js
@@ -1,3 +1,21 @@
+// var close_time; // 시간 정보
+// var close_time2 = 10; // 10초 설정
+
+// clearTimeout(close_time); // 재호출 정지
+// close_time= setTimeout("close_window()", 10000);
+// // 1/1000 초 지정, 바로 시작
+// show_time(); // 실시간 시간 보여주기
+
+// function show_time(){
+//     let divClock = document.getElementById('Time');
+//     divClock.innerText = close_time2; // 10초 삽입 시작
+//     close_time2--; // 1초씩 감소
+//     setTimeout(show_time, 1000); //1초마다 갱신
+// }
+// function close_window() { // 함수 정의
+// window.close(); // 윈도우 닫기
+// }
+
 function pop_up() {
     window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
 }
@@ -28,3 +46,7 @@ function over(obj) {
 function out(obj) {
     obj.src ="../Web_main 20220987/image/logo_steam.svg";
 }
+
+//const over = (obj) => {
+//      obj.src = "image/Logo.png";   
+//};
